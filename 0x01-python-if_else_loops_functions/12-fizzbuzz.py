f7cfc0ac80728de459724e906476d60cb34eba18
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-09 12:27:58 -0700

    0x01-python-if_else_loops_functions #12 add sol

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0:
+            print("Fizz", end="")
+        if number % 5 == 0:
+            print("Buzz", end="")
+        elif number % 3 != 0:
+            print("{}".format(number), end="")
+        print(" ", end="")
