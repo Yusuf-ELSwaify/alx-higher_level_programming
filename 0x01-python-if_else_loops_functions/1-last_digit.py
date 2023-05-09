fba46f52d9ee398526b7d52c212a93cfda1e4c49
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-09 05:44:51 -0700

    0x01-python-if_else_loops_functions: #2:#4 remove f-string and put .format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = int((abs(number) % 10) * (number / abs(number)))
+digit = number - (int(number / 10) * 10)
 
 print(f"Last digit of {number:d} is {digit:d} and is", end=" ")
 if digit == 0:
     print("0")
-if digit > 5:
+elif digit > 5:
     print("greater than 5")
 else:
     print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
     if letter is not ord('q') and letter is not ord('e'):
-        print(f"{letter:c}", end="")
+        print(f"{:c}".format(letter), end="")
