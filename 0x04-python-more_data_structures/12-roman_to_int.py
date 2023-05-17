482915f6d875be5082380b5853f57d9034ca6b32
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-17 14:51:18 -0700

    0x04-python-more_data_structures #6:#12 add sols

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    prev = 0
+
+    for i in roman_string[::-1]:
+        value = romans[i]
+        res += value * 1 if prev <= value else -1
+        prev = value
+    return res
