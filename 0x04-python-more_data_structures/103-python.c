bca5ff2e41ef3a83adfeec68e217ba07085ffb6f
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-17 17:48:46 -0700

    0x04-python-more_data_structures #103 remove not allowed macros/functions

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,24 +1,5 @@
 #include <Python.h>
 /**
- * print_python_list - prints some basic info about Python lists
- * @p: A PyObject list.
- */
-void print_python_list(PyObject *p)
-{
-	int i = 0, size = Py_SIZE(p);
-	PyObject *obj;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
-	while (i < size)
-	{
-		obj = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, Py_TYPE(obj)->tp_name);
-		i++;
-	}
-}
-/**
  * print_python_bytes - Prints info about Python byte objects
  * @p: A PyObject list
  */
@@ -33,17 +14,40 @@ void print_python_bytes(PyObject *p)
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	size = PyBytes_Size(p);
-	str = PyBytes_AsString(p);
+	size = ((PyVarObject *)p)->ob_size;
 	printf("  size: %d\n", size);
+	str = ((PyBytesObject *)p)->ob_sval;
 	printf("  trying string: %s\n", str);
+
 	size = size > 10 ? 10 : size + 1;
 	printf("  first %d bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
-		printf("%02x", (unsigned char)str[i]);
+		printf("%02hhx", str[i]);
 		if (i != size - 1)
 			printf(" ");
 	}
 	printf("\n");
+}
+/**
+ * print_python_list - prints some basic info about Python lists
+ * @p: A PyObject list.
+ */
+void print_python_list(PyObject *p)
+{
+	int i = 0, size = ((PyVarObject *)p)->ob_size;
+	PyListObject *list = (PyListObject *)p;
+	const char *type;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %zd\n", list->allocated);
+	while (i < size)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
+		i++;
+	}
 }
