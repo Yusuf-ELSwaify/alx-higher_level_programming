f381ee97391936cfd7e098eab02ab668488cb1b2
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-29 10:27:25 -0700

    0x05-python-exceptions #2:#4 added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
+    counter = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
+            counter += 1
         except IndexError:
-            i -= 1
             break
     print()
-    return i + 1
+    return counter
