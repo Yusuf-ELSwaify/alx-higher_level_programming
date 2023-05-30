ec7db76319f31fbc953188cbb2b4b53b8ef28084
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-30 06:09:03 -0700

    0x06-python-classes #0 add main

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """Define class Square"""
+
+
 class Square:
     """Blueprint for a square"""
     pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
