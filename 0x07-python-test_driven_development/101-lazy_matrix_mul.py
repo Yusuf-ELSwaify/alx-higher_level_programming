f08c8a0a005afb863ef4b372d9cb7c9f18c7175a
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-06-04 05:16:29 -0700

    0x07-python-test_driven_development 101 solution and tests added

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a function to matrix multiplication using numpy."""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """matrix multiplication using numpy."""
+    return (np.matmul(m_a, m_b))
