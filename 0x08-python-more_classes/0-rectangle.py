1842cf608b2204c8433ff3c1f1226ee708bd17a7
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-06-05 14:16:41 -0700

    0x08-python-more_classes #0, #1 added

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define class Rectangle"""
+
+
+class Rectangle:
+    """Blueprint for a rectangle"""
+    pass
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
