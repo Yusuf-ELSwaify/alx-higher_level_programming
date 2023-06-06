7db1f0f6abaa4bc3eba1c5923e51b3d6b6d06354
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-06-06 05:06:03 -0700

    0x09-python-everything_is_object #19 remove main

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def copy_list(list):
     return list.copy()
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3]
-    print(my_list)
-
-    new_list = copy_list(my_list)
-
-    print(my_list)
-    print(new_list)
-
-    print(new_list == my_list)
-    print(new_list is my_list)
