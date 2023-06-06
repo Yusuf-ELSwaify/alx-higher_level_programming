61a9d5e29a2e42c6a04d7168cc8577c9d9b7a2e3
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-06-06 04:58:39 -0700

    0x09-python-everything_is_object #15, #16, #17 and #19 update

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-def copy_list(l):
-    return l.copy()
+def copy_list(list):
+    return list.copy()
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3]
+    print(my_list)
+
+    new_list = copy_list(my_list)
+
+    print(my_list)
+    print(new_list)
+
+    print(new_list == my_list)
+    print(new_list is my_list)
