1a7a87cd7306f43578bdbd09dcea8163e6f0c908
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-06-13 19:44:11 -0700

    0x0B-python-input_output enhance #7 code style and remove main from #8 due to import inside it

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,17 +5,3 @@
 def class_to_json(obj):
     """Return the dictionary represntation of a simple class."""
     return obj.__dict__
-
-
-if __name__ == "__main__":
-    MyClass = __import__('8-my_class').MyClass
-    class_to_json = __import__('8-class_to_json').class_to_json
-
-    m = MyClass("John")
-    m.number = 89
-    print(type(m))
-    print(m)
-
-    mj = class_to_json(m)
-    print(type(mj))
-    print(mj)
