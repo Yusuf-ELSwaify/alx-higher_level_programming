913c92a7886a40bd84a51381154ce3b0ee41315e
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-07-20 19:48:56 -0700

    0x0E-SQL_more_queries #10:13 added

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT c.id AS id , c.name AS name, s.name AS name
-FROM cities as c
-JOIN states as s ON c.state_id = s.id;
+FROM cities AS c
+JOIN states AS s ON c.state_id = s.id;
